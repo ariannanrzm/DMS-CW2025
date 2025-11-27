41ce19dfc6d16b38258a129651d49c5997c91e34
ariannanrzm <199861640+ariannanrzm@users.noreply.github.com>
2025-11-27 22:40:24 +0800

    Refactor: fix NullPointerException in MockGuiController

--- src/test/java/com/comp2042/MockGuiController.java
+++ src/test/java/com/comp2042/MockGuiController.java
@@ -1,6 +1,8 @@
 package com.comp2042;
 
+import com.comp2042.game.board.ClearRow;
 import com.comp2042.game.board.ViewData;
+import com.comp2042.game.controller.GameController;
 import com.comp2042.game.events.InputEventListener;
 import com.comp2042.ui.GuiController;
 import javafx.beans.property.IntegerProperty;
@@ -22,6 +24,17 @@ public class MockGuiController extends GuiController {
         // Do nothing (override FXML/JavaFX initialization)
     }
 
+    /**
+     * CRITICAL FIX:
+     * We override this method to prevent the parent class from executing:
+     * inputHandler.attachTo(gamePanel);
+     * Since 'gamePanel' is null in this mock, the parent method causes a NullPointerException.
+     */
+    @Override
+    public void setGameController(GameController controller) {
+        // Do nothing. We don't need the real InputHandler in unit tests.
+    }
+
     @Override
     public void setEventListener(InputEventListener eventListener) {
         super.setEventListener(eventListener);
@@ -39,6 +52,11 @@ public class MockGuiController extends GuiController {
     }
 
     @Override
+    public void refreshBrick(ViewData brick) {
+        this.latestViewData = brick;
+    }
+
+    @Override
     public void bindScore(IntegerProperty integerProperty) {
         // Skip GUI binding
     }
@@ -49,8 +67,19 @@ public class MockGuiController extends GuiController {
     }
 
     @Override
+    public void resetTimeline() {
+        // Do nothing during tests to avoid "Toolkit not initialized" errors
+    }
+
+    @Override
+    public void showNotificationIfRowsCleared(ClearRow clearRow) {
+        // Do nothing during tests
+    }
+
+    @Override
     public void newGame(ActionEvent actionEvent) {
-        // Skip GUI new game behavior
+        // Reset internal test state
+        this.gameOverCalled = false;
     }
 
     @Override
